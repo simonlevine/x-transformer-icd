0c40cb3883608e3aebf02b116e9632c15ae6712b
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2020-06-20 09:39:16 -0700

    Fixed indexing mistake

--- src/pipeline/format_data_for_training.py
+++ src/pipeline/format_data_for_training.py
@@ -19,15 +19,23 @@ def main():
 
 def construct_datasets():
     logger.info("Ingesting MIMIC data...")
-    diagnosis_df = pd.read_csv(DIAGNOSIS_CSV_FP, usecols=["HADM_ID", "ICD9_CODE", "SEQ_NUM"])
-    note_events_df = pd.read_csv(NOTE_EVENTS_CSV_FP, usecols=["HADM_ID", "TEXT", "CATEGORY"])
-    note_events_df = note_events_df[note_events_df.CATEGORY == "Discharge summary"]
+    diagnosis_df = pd.read_csv(DIAGNOSIS_CSV_FP, usecols=[
+                               "HADM_ID", "ICD9_CODE", "SEQ_NUM"])
+    # merging with description...
+    icd_key_df = pd.read_csv(ICD_KEY_FP, usecols=['ICD9_CODE', 'LONG_TITLE'])
+    diagnosis_df = diagnosis_df.merge(
+        icd_key_df, left_on=['ICD9_CODE'], right_on=['ICD9_CODE'])
+    note_events_df = pd.read_csv(NOTE_EVENTS_CSV_FP, usecols=[
+                                 "HADM_ID", "TEXT", "CATEGORY"])
+    note_events_df = note_events_df[note_events_df.CATEGORY ==
+                                    "Discharge summary"]
     df_raw = note_events_df.merge(diagnosis_df).dropna()
     logger.info("Pivoting to group related entries and ICD codes...")
     df = df_raw.groupby(["HADM_ID", "TEXT"]).agg({
-        "ICD9_CODE": set, "SEQ_NUM": set,
+        "ICD9_CODE": set,
+        "SEQ_NUM": set,
+        "LONG_TITLE": set,
     }).reset_index()
-
     logger.info("Splitting into test/train...")
     df_train = df.sample(frac=0.66, random_state=42)
     df_test = df.drop(df_train.index)
