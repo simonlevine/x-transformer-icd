c5565fe31fc6bc61686a3d5e6e866e5ffd4c1884
Jeremy Fisher <jeremyfisher@Jeremys-MacBook-Pro.local>
2020-06-20 10:08:04 -0400

    Convert model to ONNX, not working yet

--- src/pipeline/train_model_kiss.py
+++ src/pipeline/train_model_kiss.py
@@ -6,6 +6,9 @@ and identifying pain points"""
 import typing as t
 IcdCode = int
 
+import collections
+import json
+from pathlib import Path
 import numpy as np
 import pandas as pd
 from sklearn.decomposition import TruncatedSVD
@@ -13,16 +16,36 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
+import skl2onnx
 from skl2onnx import convert_sklearn
 from skl2onnx.common.data_types import StringTensorType
 
+
 NOTES2DIAGNOSIS_ICD_TRAIN_FP = "./intermediary-data/notes2diagnosis-icd-train.json.gz"
-NOTES2DIAGNOSIS_ICD_TEST_FP = "./intermediary-data/notes2diagnosis-icd-test.json.gz"
-MODEL_WEIGHTS_OUT_FP = "./app/kiss_model.onnx"
+MODEL_WEIGHTS_OUT_FP = "./app/assets/kiss_model.onnx"
+MODEL_METADATA_FP = MODEL_WEIGHTS_OUT_FP + ".metadata.json"
+
+
+def main(subsample=True):
+    df_train = pd.read_json(NOTES2DIAGNOSIS_ICD_TRAIN_FP, orient="split")
+    if subsample:
+        df_train = df_train.sample(n=10_000)
+    X_train, y_train, icd9_codes_relevant = dataframe2tensor(df_train, num_most_common=10)
+    model = get_model()
+    model.fit(X_train, y_train)
+    model_onnx = convert_model2onnx(model, X_train)
+    with open(MODEL_WEIGHTS_OUT_FP, "wb") as f_out , \
+         open(MODEL_METADATA_FP, "w") as f_metadata_out:
+        f_out.write(model_onnx.SerializeToString())
+        json.dump({
+            "model_fname": Path(MODEL_WEIGHTS_OUT_FP).name,
+            "icd9_codes_relevant": icd9_codes_relevant
+        }, f_metadata_out)
+
 
 def dataframe2tensor(df: pd.DataFrame,
-                     num_most_common:int,
-                     icd_codes:t.List[IcdCode]=None
+                     num_most_common: int,
+                     icd_codes: t.List[IcdCode] = None
                      ) -> np.ndarray:
     """convert a dataframe, cleaned up in the previous step,
     into a tensor representing the most common codes
@@ -33,37 +56,39 @@ def dataframe2tensor(df: pd.DataFrame,
         icd_codes: the relevant codes, which also specify their ordering
     """
     if icd_codes is None:
-        icd9_codes = list(df.ICD9_CODE.unique())
-    ddf = pd.DataFrame(index=df.HADM_ID.unique(), columns=["TEXT"] + icd9_codes).fillna(False)
-    for idx, row in df.iterrows():
-        ddf.loc[row.HADM_ID, "TEXT"] = row.TEXT
-        ddf.loc[row.HADM_ID, row.ICD9_CODE] = True
-    X = ddf[["TEXT"]].values
-    y = ddf[icd9_codes[0]].values.astype("int64")
+        icd_distribution = collections.Counter(sum(df.ICD9_CODE, []))
+        icd_codes, _ = list(zip(*icd_distribution.most_common(num_most_common)))
+        icd_codes = list(icd_codes)
+    else:
+        assert len(icd_codes) == num_most_common
+    for icd_code in icd_codes:
+        df[icd_code] = df.ICD9_CODE.apply(lambda codes_associated_with_row: icd_code in codes_associated_with_row).astype("int64")
+    df = df[df[icd_codes].sum(axis=1) > 0]  # filter out entries without codes of interest
+    X = df["TEXT"].values
+    y = df[icd_codes].values
     return X, y, icd_codes
 
 
-def main(subsample=True):
-    df_train = pd.read_json(NOTES2DIAGNOSIS_ICD_TRAIN_FP)
-    # df_test = pd.read_json(NOTES2DIAGNOSIS_ICD_TEST_FP)
-    if subsample:
-        icd_codes_most_frequent = set(df_train["ICD9_CODE"].value_counts().index[:10])
-        df_train = df_train[df_train.ICD9_CODE.isin(most_frequent)].sample(n=10_000)
-    model = Pipeline(steps=[
-        ("reshape", ColumnTransformer([
-            ("vectorize", TfidfVectorizer(), 0),
-        ])),
+def get_model():
+    return Pipeline(steps=[
+        ("vectorize", TfidfVectorizer()),
         ("dimensionality_reduction", TruncatedSVD(n_components=50)),
         ("classifier", RandomForestClassifier()),
     ])
-    X_train, y_train, icd9_codes_relevant = dataframe2tensor(df_train, 10)
-    # X_test, y_test, _ = dataframe2tensor(df_test, 10, icd9_codes_relevant)
-    model_onx = convert_sklearn(
-        pipeline,
+
+
+
+def convert_model2onnx(model, training_data):
+    return skl2onnx.to_onnx(
+        model,
+        training_data,
         "autoicd-kiss",
-        initial_types=[("free_text_input", StringTensorType(shape=[None, 1]))],
+        initial_types=[
+            ( "free_text_input", StringTensorType(shape=[None ,1]) )
+        ],
         options={
-            # stolen shamelessly from the ONNX docs: http://onnx.ai/sklearn-onnx/auto_examples/plot_tfidfvectorizer.html
+            # stolen shamelessly from the ONNX docs:
+            # http://onnx.ai/sklearn-onnx/auto_examples/plot_tfidfvectorizer.html
             TfidfVectorizer: {
                 "separators": [
                     ' ', '.', '\\?', ',', ';', ':', '!',
@@ -73,8 +98,7 @@ def main(subsample=True):
             }
         }
     )
-    with open(MODEL_WEIGHTS_OUT_FP, "wb") as f_out:
-        f_out.write(model_onx.SerializeToString())
+
 
 if __name__ == "__main__":
     main()
