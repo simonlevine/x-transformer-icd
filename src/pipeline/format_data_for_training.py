35ab3ce1f956a8cd10c5dbbe5feaf9ced0e3a619
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2020-06-20 10:43:00 -0700

    Adding ICD10 General Equivalency Mapping logic
    Added option to construct ICD10 equivalent dataset (defaults to icd10) using ICD_GEM csv added to data folder.

--- src/pipeline/format_data_for_training.py
+++ src/pipeline/format_data_for_training.py
@@ -6,7 +6,8 @@ from loguru import logger
 
 DIAGNOSIS_CSV_FP = "../data/mimiciii-14/DIAGNOSES_ICD.csv.gz"
 NOTE_EVENTS_CSV_FP = "../data/mimiciii-14/NOTEEVENTS.csv.gz"
-
+ICD9_KEY_FP = "../data/mimiciii-14/D_ICD_DIAGNOSES.csv.gz"
+ICD_GEM_FP = "../data/ICD_general_equivalence_mapping.csv"
 
 def main():
     df_train, df_test = construct_datasets()
@@ -17,25 +18,50 @@ def main():
         df_.to_json(fp_out, orient="split")
 
 
-def construct_datasets():
+def construct_datasets(icd_version='10'):
     logger.info("Ingesting MIMIC data...")
     diagnosis_df = pd.read_csv(DIAGNOSIS_CSV_FP, usecols=[
                                "HADM_ID", "ICD9_CODE", "SEQ_NUM"])
     # merging with description...
-    icd_key_df = pd.read_csv(ICD_KEY_FP, usecols=['ICD9_CODE', 'LONG_TITLE'])
+    icd9_key_df = pd.read_csv(ICD9_KEY_FP, usecols=['ICD9_CODE', 'LONG_TITLE'])
+    #pulling general equivalence mapping...
+    icd_gem_df = pd.read_csv(ICD_GEM_FP, sep='|', header=None).rename(
+        columns=dict(zip((0, 1, 2), ('ICD9_CODE', 'ICD10_CODE', 'LONG_TITLE'))))
+
+    icd_gem_df['ICD9_CODE'] = icd_gem_df['ICD9_CODE'].str.replace('.', '')
+
     diagnosis_df = diagnosis_df.merge(
         icd_key_df, left_on=['ICD9_CODE'], right_on=['ICD9_CODE'])
+
+    if icd_version == '10':
+        logger.info("Converting ICD9 to ICD10...")
+
+        diagnosis_df = diagnosis_df.merge(
+            icd_gem, left_on=['ICD9_CODE'], right_on=['ICD9_CODE'])
+        diagnosis_df = diagnosis_df.drop(columns=['ICD9_CODE', 'LONG_TITLE_x']).rename(
+            columns={'LONG_TITLE_y': 'LONG_TITLE'})
+
     note_events_df = pd.read_csv(NOTE_EVENTS_CSV_FP, usecols=[
                                  "HADM_ID", "TEXT", "CATEGORY"])
     note_events_df = note_events_df[note_events_df.CATEGORY ==
                                     "Discharge summary"]
     df_raw = note_events_df.merge(diagnosis_df).dropna()
     logger.info("Pivoting to group related entries and ICD codes...")
-    df = df_raw.groupby(["HADM_ID", "TEXT"]).agg({
-        "ICD9_CODE": set,
-        "SEQ_NUM": set,
-        "LONG_TITLE": set,
-    }).reset_index()
+
+    if icd_version == '10':
+        df = df_raw.groupby(["HADM_ID", "TEXT"]).agg({
+            "ICD10_CODE": set,
+            "SEQ_NUM": set,
+            "LONG_TITLE": set,
+        }).reset_index()
+
+    else:
+        df = df_raw.groupby(["HADM_ID", "TEXT"]).agg({
+            "ICD9_CODE": set,
+            "SEQ_NUM": set,
+            "LONG_TITLE": set,
+        }).reset_index()
+
     logger.info("Splitting into test/train...")
     df_train = df.sample(frac=0.66, random_state=42)
     df_test = df.drop(df_train.index)
