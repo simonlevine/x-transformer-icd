b4612a8b8a0f0ac0847fe087b602312050ed6893
Jeremy Fisher <jeremyfisher@Jeremys-MacBook-Pro.local>
2020-06-20 14:27:09 -0400

    Reduce code surface, improve variable names

--- src/pipeline/format_data_for_training.py
+++ src/pipeline/format_data_for_training.py
@@ -22,22 +22,17 @@ def construct_datasets(icd_version='10'):
     logger.info("Ingesting MIMIC data...")
     diagnosis_df = pd.read_csv(DIAGNOSIS_CSV_FP, usecols=[
                                "HADM_ID", "ICD9_CODE", "SEQ_NUM"])
-    # merging with description...
-    icd9_key_df = pd.read_csv(ICD9_KEY_FP, usecols=['ICD9_CODE', 'LONG_TITLE'])
-    #pulling general equivalence mapping...
-    icd_gem_df = pd.read_csv(ICD_GEM_FP, sep='|', header=None).rename(
-        columns=dict(zip((0, 1, 2), ('ICD9_CODE', 'ICD10_CODE', 'LONG_TITLE'))))
-
-    icd_gem_df['ICD9_CODE'] = icd_gem_df['ICD9_CODE'].str.replace('.', '')
-
+    icd9_long_description_df = pd.read_csv(ICD9_KEY_FP, usecols=['ICD9_CODE', 'LONG_TITLE'])
+    icd_eqivalence_mapping_df = pd.read_csv(ICD_GEM_FP, sep='|', header=None).rename(
+        columns={0: 'ICD9_CODE', 1: 'ICD10_CODE', 2: 'LONG_TITLE'})
+    icd_eqivalence_mapping_df['ICD9_CODE'] = icd_eqivalence_mapping_df['ICD9_CODE'].str.replace('.', '')
     diagnosis_df = diagnosis_df.merge(
-        icd_key_df, left_on=['ICD9_CODE'], right_on=['ICD9_CODE'])
+        icd9_long_description_df, left_on=['ICD9_CODE'], right_on=['ICD9_CODE'])
 
     if icd_version == '10':
         logger.info("Converting ICD9 to ICD10...")
-
         diagnosis_df = diagnosis_df.merge(
-            icd_gem, left_on=['ICD9_CODE'], right_on=['ICD9_CODE'])
+            icd_eqivalence_mapping_df, left_on=['ICD9_CODE'], right_on=['ICD9_CODE'])
         diagnosis_df = diagnosis_df.drop(columns=['ICD9_CODE', 'LONG_TITLE_x']).rename(
             columns={'LONG_TITLE_y': 'LONG_TITLE'})
 
@@ -46,22 +41,14 @@ def construct_datasets(icd_version='10'):
     note_events_df = note_events_df[note_events_df.CATEGORY ==
                                     "Discharge summary"]
     df_raw = note_events_df.merge(diagnosis_df).dropna()
+
     logger.info("Pivoting to group related entries and ICD codes...")
-
-    if icd_version == '10':
-        df = df_raw.groupby(["HADM_ID", "TEXT"]).agg({
-            "ICD10_CODE": set,
-            "SEQ_NUM": set,
-            "LONG_TITLE": set,
-        }).reset_index()
-
-    else:
-        df = df_raw.groupby(["HADM_ID", "TEXT"]).agg({
-            "ICD9_CODE": set,
-            "SEQ_NUM": set,
-            "LONG_TITLE": set,
-        }).reset_index()
-
+    df = df_raw.groupby(["HADM_ID", "TEXT"]).agg({
+        ("ICD10_CODE" if icd_version == '10' else "ICD9_CODE"): set,
+        "SEQ_NUM": set,
+        "LONG_TITLE": set,
+    }).reset_index()
+    
     logger.info("Splitting into test/train...")
     df_train = df.sample(frac=0.66, random_state=42)
     df_test = df.drop(df_train.index)
