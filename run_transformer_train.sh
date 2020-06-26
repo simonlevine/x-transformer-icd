ceb2cafd80e7845719c82e9519af371c299155b8
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2020-06-26 16:56:15 -0700

    Update run_transformer_train.sh
    Need to specify 2070 train/val parameters.
    Need to specify hyperparameters for MIMIC data.

--- run_transformer_train.sh
+++ run_transformer_train.sh
@@ -1,58 +1,78 @@
 #!/bin/bash
 
-GPID=$1
-DATASET=$2
-MODEL_TYPE=$3
+GPID=$1 #CUDA visible devices. 0 for just one 2070 GPU.
+# DATASET=$2
+DATASET=$'mimiciii-14'
+# MODEL_TYPE=$3
 INDEXER_NAME=$4 # pifa-tfidf-s0 ||| pifa-neural-s0 ||| text-emb-s0
-if [ ${MODEL_TYPE} == "bert" ]; then
-    MODEL_NAME=bert-large-cased-whole-word-masking
-elif [ ${MODEL_TYPE} == "roberta" ]; then
-    MODEL_NAME=roberta-large
-elif [ ${MODEL_TYPE} == "xlnet" ]; then
-    MODEL_NAME=xlnet-large-cased
-else
-    echo "unknown MODEL_TYPE! [ bert | robeta | xlnet ]"
-    exit
-fi
+
+
+MODEL_NAME='Bio_ClinicalBERT'
+MODEL_TYPE=$'bert'
+
+# if [ ${MODEL_TYPE} == "bert" ]; then
+#     MODEL_NAME=bert-large-cased-whole-word-masking
+# elif [ ${MODEL_TYPE} == "roberta" ]; then
+#     MODEL_NAME=roberta-large
+# elif [ ${MODEL_TYPE} == "xlnet" ]; then
+#     MODEL_NAME=xlnet-large-cased
+# else
+#     echo "unknown MODEL_TYPE! [ bert | robeta | xlnet ]"
+#     exit
+# fi
+
+
 OUTPUT_DIR=save_models/${DATASET}
 PROC_DATA_DIR=${OUTPUT_DIR}/proc_data
 MAX_XSEQ_LEN=128
 
-# Nvidia 2080Ti (11Gb), fp32
-PER_DEVICE_TRN_BSZ=8
-PER_DEVICE_VAL_BSZ=16
-GRAD_ACCU_STEPS=4
+#SHOULD ADD SOMETHING HERE FOR A 2070:
 
-# Nvidia V100 (16Gb), fp32
-PER_DEVICE_TRN_BSZ=16
-PER_DEVICE_VAL_BSZ=32
-GRAD_ACCU_STEPS=2
+
+# # Nvidia 2080Ti (11Gb), fp32
+# PER_DEVICE_TRN_BSZ=8
+# PER_DEVICE_VAL_BSZ=16
+# GRAD_ACCU_STEPS=4
+
+# # Nvidia V100 (16Gb), fp32
+# PER_DEVICE_TRN_BSZ=16
+# PER_DEVICE_VAL_BSZ=32
+# GRAD_ACCU_STEPS=2
+
+
+#HYPERPARAMETERS for MIMIC:
+MAX_STEPS=1000
+WARMUP_STEPS=100
+LOGGING_STEPS=50
+LEARNING_RATE=5e-5
+
 
 # set hyper-params by dataset
-if [ ${DATASET} == "Eurlex-4K" ]; then
-    MAX_STEPS=1000
-    WARMUP_STEPS=100
-    LOGGING_STEPS=50
-    LEARNING_RATE=5e-5
-elif [ ${DATASET} == "Wiki10-31K" ]; then
-    MAX_STEPS=1400
-    WARMUP_STEPS=100
-    LOGGING_STEPS=50
-    LEARNING_RATE=5e-5
-elif [ ${DATASET} == "AmazonCat-13K" ]; then
-    MAX_STEPS=20000
-    WARMUP_STEPS=2000
-    LOGGING_STEPS=100
-    LEARNING_RATE=8e-5
-elif [ ${DATASET} == "Wiki-500K" ]; then
-    MAX_STEPS=80000
-    WARMUP_STEPS=1000
-    LOGGING_STEPS=100
-    LEARNING_RATE=6e-5
-else
-    echo "dataset not support [ Eurlex-4K | Wiki10-31K | AmazonCat-13K | Wiki-500K ]"
-    exit
-fi
+
+# if [ ${DATASET} == "Eurlex-4K" ]; then
+#     MAX_STEPS=1000
+#     WARMUP_STEPS=100
+#     LOGGING_STEPS=50
+#     LEARNING_RATE=5e-5
+# elif [ ${DATASET} == "Wiki10-31K" ]; then
+#     MAX_STEPS=1400
+#     WARMUP_STEPS=100
+#     LOGGING_STEPS=50
+#     LEARNING_RATE=5e-5
+# elif [ ${DATASET} == "AmazonCat-13K" ]; then
+#     MAX_STEPS=20000
+#     WARMUP_STEPS=2000
+#     LOGGING_STEPS=100
+#     LEARNING_RATE=8e-5
+# elif [ ${DATASET} == "Wiki-500K" ]; then
+#     MAX_STEPS=80000
+#     WARMUP_STEPS=1000
+#     LOGGING_STEPS=100
+#     LEARNING_RATE=6e-5
+# else
+#     echo "dataset not support [ Eurlex-4K | Wiki10-31K | AmazonCat-13K | Wiki-500K ]"
+#     exit
+# fi
 
 MODEL_DIR=${OUTPUT_DIR}/${INDEXER_NAME}/matcher/${MODEL_NAME}
 mkdir -p ${MODEL_DIR}
