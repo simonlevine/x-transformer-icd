923a01f0f69db665880aa86afe96831602f64bbb
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2020-06-29 16:23:39 -0700

    working on local proprocessed sample dataframes

--- run_preprocess_label.sh
+++ run_preprocess_label.sh
@@ -1,23 +1,23 @@
-# !/bin/bash
-DATASET = $'mimiciii-14'
+#!/bin/bash
+DATASET=$'mimiciii-14'
 LABEL_EMB=$'pifa-tfidf'
 
 # setup label embedding feature path
 #overwritten by Simon Levine for mimic.
 
-# DATA_DIR= $/Users/simon/autoicd_local/xbert_inputs
-label_emb_inst_path= $'/Users/simon/autoicd_local/data/xbert_inputs/X.trn.npz' # ${DATA_DIR}/${DATASET}/X.trn.npz
+DATA_DIR=$'/Users/simon/autoicd_local/xbert_inputs'
+label_emb_inst_path=${DATA_DIR}/${DATASET}/X.trn.npz
 
 # construct label embedding
 OUTPUT_DIR=$'/Users/simon/autoicd_local/xbert_savedmodels' #/${DATASET}
-PROC_DATA_DIR= $'/Users/simon/autoicd_local/xbert_out/proc_data' #${OUTPUT_DIR}/proc_data
+PROC_DATA_DIR=$'/Users/simon/autoicd_local/xbert_out/proc_data' #${OUTPUT_DIR}/proc_data
 
 mkdir -p ${PROC_DATA_DIR}
  #UNSURE WHY THIS WAS COLON-QUOTED:####\
 #  :'
 python -m xbert.preprocess \
 	--do_label_embedding \
-	-i $ {DATA_DIR}/${DATASET} \
+	-i ${DATA_DIR}/${DATASET} \
     -o ${PROC_DATA_DIR} \
     -l ${LABEL_EMB} \
     -x ${label_emb_inst_path}
@@ -32,8 +32,8 @@ python -m xbert.preprocess \
 # 		-i ${PROC_DATA_DIR}/L.${LABEL_EMB}.npz \
 # 		-o ${INDEXER_DIR} --seed ${SEED}
 # done
-# #### WAS COLON-QUOTED OUT UNTIL HERE
 # # '
+# #### WAS COLON-QUOTED OUT UNTIL HERE
 
 # # construct C.[trn|tst].[label-emb].npz for training matcher
 # SEED=0
