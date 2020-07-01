a91d4f74bb39d005cf757227fa0d5012aeeed8ed
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2020-07-01 13:51:43 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@
 cd auto-icd-transformers
 conda env create -f environment.yml
 source activate pt1.2_xmlc_transformer
-(pt1.2_xmlc_transformer) pip install -e .
-(pt1.2_xmlc_transformer) python setup.py install --force
+pip install -e .
+python setup.py install --force
