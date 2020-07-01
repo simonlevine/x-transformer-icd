bdeeaf75587d4ff230fc77e190bbdcaccf669065
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2020-07-01 14:47:46 -0700

    Update xbert_preprocessing.py
    critical filename update

--- src/pipeline/xbert_preprocessing.py
+++ src/pipeline/xbert_preprocessing.py
@@ -56,8 +56,8 @@ ICD_GEM_FP = "../data/ICD_general_equivalence_mapping.csv"
 
 #output filepaths
 XBERT_LABEL_MAP_FP = '../data/intermediary-data/xbert_inputs/mimiciii-14/label_map.txt'
-XBERT_TRAIN_RAW_TEXTS_FP = '../data/intermediary-data/xbert_inputs/mimiciii-14/train_raw_labels.txt'
-XBERT_TEST_RAW_TEXTS_FP = '../data/intermediary-data/xbert_inputs/mimiciii-14/test_raw_labels.txt'
+XBERT_TRAIN_RAW_TEXTS_FP = '../data/intermediary-data/xbert_inputs/mimiciii-14/train_raw_texts.txt'
+XBERT_TEST_RAW_TEXTS_FP = '../data/intermediary-data/xbert_inputs/mimiciii-14/test_raw_texts.txt'
 XBERT_X_TRN_FP = '../data/intermediary-data/xbert_inputs/mimiciii-14/X.trn.npz'
 XBERT_X_TST_FP = '../data/intermediary-data/xbert_inputs/mimiciii-14/X.tst.npz'
 XBERT_Y_TRN_FP = '../data/intermediary-data/xbert_inputs/mimiciii-14/Y.trn.npz'
