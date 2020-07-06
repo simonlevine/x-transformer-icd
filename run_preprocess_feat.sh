8ab6f44d51c37b32132e066a6288c6bf982001e0
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2020-07-05 21:36:54 -0700

    Update run_preprocess_feat.sh

--- run_preprocess_feat.sh
+++ run_preprocess_feat.sh
@@ -9,8 +9,7 @@ MAX_XSEQ_LEN=$'128'
 MODEL_TYPE=$'bert'
 MODEL_NAME=$'emilyalsentzer/Bio_ClinicalBERT'
 
-
-DATA_DIR=../auto-icd/data/intermediary-data
+DATA_DIR=../../auto-icd/data/intermediary-data
 OUTPUT_DIR=${DATA_DIR}/xbert_outputs
 
 PROC_DATA_DIR=${OUTPUT_DIR}/proc_data
