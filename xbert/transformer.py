888f109299093f48fff5c90319a1bcc031766ae6
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2020-07-06 15:14:51 -0700

    Update transformer.py

--- xbert/transformer.py
+++ xbert/transformer.py
@@ -188,7 +188,7 @@ class TransformerMatcher(object):
             "-o",
             "--output_dir",
             default="./tmp",
-            required=True
+            required=True,
             type=str,
             help="The output directory where the model predictions and checkpoints will be written.",
         )
