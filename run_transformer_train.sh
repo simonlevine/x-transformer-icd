3c3490bedc5c41fe2281684278c125a4510af845
Jeremy Fisher <jeremy@adamsfisher.me>
2020-07-07 14:31:59 -0400

    Setting up first training stage run
    Better shebang, activate conda automatically, run transformer.py NOT in
    distributed mode -- also the line breaks were weird, so replaced those

--- run_transformer_train.sh
+++ run_transformer_train.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+source create_conda_env_as_necessary.sh
 
 GPID=${0} #CUDA visible devices. 0 for just one 2070 GPU.
 # NPROC_PER_NODE={'1'}
@@ -10,8 +12,8 @@ MODEL_NAME='emilyalsentzer/Bio_ClinicalBERT'
 MODEL_FOLDER_NAME='Bio_ClinicalBERT'
 MODEL_TYPE=$'bert'
 
-OUTPUT_DIR=~/auto-icd/saved_models/${DATASET} 
-PROC_DATA_DIR=~/auto-icd/data/intermediary-data/xbert_outputs/proc_data
+OUTPUT_DIR=../../data/intermediary-data/xbert_outputs
+PROC_DATA_DIR=../../data/intermediary-data/xbert_outputs/proc_data
 MAX_XSEQ_LEN=128
 
 #SHOULD ADD SOMETHING HERE FOR A 2070?
@@ -37,23 +39,23 @@ WARMUP_STEPS=100
 LOGGING_STEPS=50
 LEARNING_RATE=5e-5
 
-
 MODEL_DIR=${OUTPUT_DIR}/${INDEXER_NAME}/matcher/${MODEL_FOLDER_NAME}
 mkdir -p ${MODEL_DIR}
 
-
-CUDA_VISIBLE_DEVICES=${GPID} python -m xbert/transformer.py \ #This change works
-    -m ${MODEL_TYPE} -n ${MODEL_NAME} --do_train \
+python xbert/transformer.py \
+    -m ${MODEL_TYPE} \
+    -n ${MODEL_NAME} \
+    --do_train \
     -x_trn ${PROC_DATA_DIR}/X.trn.${MODEL_TYPE}.${MAX_XSEQ_LEN}.pkl \
-    -c_trn ${PROC_DATA_DIR}/C.trn.${INDEXER_NAME}.npz \ 
+    -c_trn ${PROC_DATA_DIR}/C.trn.${INDEXER_NAME}.npz \
     -o ${MODEL_DIR} \
     --per_device_train_batch_size ${PER_DEVICE_TRN_BSZ} \
     --gradient_accumulation_steps ${GRAD_ACCU_STEPS} \
     --max_steps ${MAX_STEPS} \
     --warmup_steps ${WARMUP_STEPS} \
     --learning_rate ${LEARNING_RATE} \
-    --logging_steps ${LOGGING_STEPS} \
-    |& tee ${MODEL_DIR}/log.txt
+    --overwrite_output_dir \
+    --logging_steps ${LOGGING_STEPS}  |& tee ${MODEL_DIR}/log.txt
 
 
 # # train
