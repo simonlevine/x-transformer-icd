4b631d38bbb6c67a509d1761cee25eb328ccb973
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2020-07-11 20:08:42 -0700

    Update transformer.py

--- xbert/transformer.py
+++ xbert/transformer.py
@@ -414,7 +414,7 @@ class TransformerMatcher(object):
                     c_pred = outputs[0]
 
                 # compute loss
-                c_eval = np.array(C_eval_true[inst_idx].toarray()) #BUG C_eval_true[inst_idx] does not exist.
+                c_eval = np.array(C_eval_true[inst_idx].toarray()) #BUG index does not exist.
                 c_eval = torch.tensor(c_eval, dtype=torch.float).to(args.device)
                 loss = self.loss_fn(c_pred, c_eval)
 
@@ -559,7 +559,7 @@ class TransformerMatcher(object):
 
                 # compute loss, average across multi-gpu
                  # BUG indx is too large
-                labels = np.array(X_trn[inst_idx].toarray())
+                labels = np.array(C_trn[inst_idx].toarray())
                 labels = torch.tensor(labels, dtype=torch.float).to(args.device)
                 loss = self.loss_fn(logits, labels)
 
