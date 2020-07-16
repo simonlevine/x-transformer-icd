93e7f9ec97f239127a5d9a6c4c9fe0b298d7ab01
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2020-07-16 13:27:15 -0700

    Update run_transformer_predict.sh
    - made fixes for single linear ranker training + prediction

--- run_transformer_predict.sh
+++ run_transformer_predict.sh
@@ -5,18 +5,19 @@ DATA_DIR=../../data/intermediary-data/xbert_inputs
 
 LABEL_NAME = $'pifa-tfidf-s0'
 MODEL_NAME = $'emilyalsentzer/Bio_ClinicalBERT'
+MODEL_FOLDER_NAME='Bio_ClinicalBERT'
 EXP_NAME=${DATASET}.final
 
 PRED_NPZ_PATHS=""
-OUTPUT_DIR=saved_models/${DATASET}/${LABEL_NAME}
+OUTPUT_DIR=../../data/intermediary-data/xbert_outputs/${LABEL_NAME}
 INDEXER_DIR=${OUTPUT_DIR}/indexer
-MATCHER_DIR=${OUTPUT_DIR}/matcher/${MODEL_NAME}
-RANKER_DIR=${OUTPUT_DIR}/ranker/${MODEL_NAME}
+MATCHER_DIR=${OUTPUT_DIR}/matcher/${MODEL_FOLDER_NAME}
+RANKER_DIR=${OUTPUT_DIR}/ranker/${MODEL_FOLDER_NAME}
 mkdir -p ${RANKER_DIR}
 
 # train linear ranker
 python -m xbert.ranker train \
-    -x1 ${DATA_DIR}/X.trn.npz \
+    -x1 ${DATASET}/${DATA_DIR}/X.trn.npz \
     -x2 ${MATCHER_DIR}/trn_embeddings.npy \
     -y ${DATA_DIR}/Y.trn.npz \
     -z ${MATCHER_DIR}/C_trn_pred.npz \
@@ -34,14 +35,12 @@ python -m xbert.ranker predict \
     -z ${MATCHER_DIR}/C_tst_pred.npz \
     -f 0 -t noop
 
-# append the prediction path (changed to just one here.)
-PRED_NPZ_PATHS="${PRED_NPZ_PATHS} ${PRED_NPZ_PATH}"
 
 # final eval
 EVAL_DIR=results_transformer-large
 mkdir -p ${EVAL_DIR}
 python -u -m xbert.evaluator \
     -y ${DATA_DIR}/Y.tst.npz \
-    -e -p ${PRED_NPZ_PATHS} \
+    -e -p ${PRED_NPZ_PATH} \
     |& tee ${EVAL_DIR}/${EXP_NAME}.txt
 
