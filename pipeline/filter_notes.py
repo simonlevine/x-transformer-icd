468ddb102e72eab9cc22b33663819952dbf53df2
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2020-08-05 17:29:28 -0700

    Update filter_notes.py

--- pipeline/filter_notes.py
+++ pipeline/filter_notes.py
@@ -79,7 +79,7 @@ def preprocess_and_clean_notes(notes_df: pd.DataFrame) -> pd.DataFrame:
     """
     logger.info(
         "Removing de-id token, punctuation, admin language and other cruft")
-    with tqdm(total=2+len(ADMIN_LANGUAGE)+6) as pbar:
+    with tqdm(total=3+len(ADMIN_LANGUAGE)+6) as pbar:
         # notes_df["TEXT"] = notes_df["TEXT"].str.lower()
         # pbar.update(1)
         notes_df["TEXT"] = notes_df["TEXT"].replace(r"\[.*?\]", "", regex=True)
